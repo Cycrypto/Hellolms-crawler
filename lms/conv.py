fb8615671a597c6df131c9b76472b0b06ed9c035
Cycrypto <jh01love@naver.com>
2021-09-10 13:29:23 +0900

    #3 Notification list and count finished

--- lms/conv.py
+++ lms/conv.py
@@ -4,6 +4,7 @@ from lms.session import GetSession
 
 import json
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 
@@ -20,21 +21,34 @@ class Notifications:
         inform_html = self.session.post(settings.NOTIFICATION_URL, data={'start': start, 'display': display})
         soup = BeautifulSoup(inform_html.text, 'html.parser')
         content = soup.find_all('div', {'class': 'notification_content'})   # 콘텐츠 접근
-        print(len(content))
+
         for con in content:
-            title = con.find('div', {'class': 'notification_subject'}).get_text().strip().replace("\r", '').replace(
-                "\n\n", '')
-            body = con.find("div", {'class': 'notification_text'}).get_text().strip().replace("\r", '').replace(
-                "\n", '') \
-                .replace("                                           ", ' ')
-            date = con.find("div", {'class': 'notification_day'}).get_text().strip().replace("\r", '').replace(
-                "\n\n", '')
-            print(title, body, date)
+            try:
+                title = con.find('div', {'class': 'notification_subject'}).get_text().\
+                    strip().replace("\r", '').replace("\n\n", '')
+
+            except AttributeError:
+                title = '-'
+
+            try:        # 내용 받아오기
+                body = con.find("div", {'class': 'notification_text'}).get_text().\
+                    strip().replace("\r", '').replace("\n", '').replace("                                           ", ' ')
+
+            except AttributeError:
+                body = '-'
+
+            try:        # 날짜 받아오기
+                date = con.find("div", {'class': 'notification_day'}).get_text().\
+                    strip().replace("\r", '').replace("\n\n", '')
+
+            except AttributeError:
+                date = '-'
+
             self.return_form['title'].append(title)
             self.return_form['content'].append(body)
-            self.return_form['title'].append(date)
+            self.return_form['date'].append(date)
 
-            return self.return_form
+        return pd.DataFrame(self.return_form)
 
     def getUnreadCnt(self, start=1, display=5) -> str:
         html = self.session.post(settings.NOTIFICATION_COUNT_URL,  data={'start': start, 'display': display})
