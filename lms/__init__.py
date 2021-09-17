7e615a7ffa44b70d2b0ff2650a9610e5125f21f6
Cycrypto <jh01love@naver.com>
2021-09-17 19:57:39 +0900

    #5 - debug option fix is finished

--- lms/__init__.py
+++ lms/__init__.py
@@ -10,25 +10,29 @@ formatter = logging.Formatter('[%(levelname)s] - %(message)s')
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
+_DEBUG = True   # 디버그 모드 생성
 
 
 def _LOG(msgtype, sender, msg):
-    if msgtype == 'debug':
-        logger.setLevel(logging.DEBUG)
-        logger.debug(f'from [{sender}], {msg}')
-
-    elif msgtype == 'info':
-        logger.setLevel(logging.INFO)
-        logger.info(f'from [{sender}], {msg}')
-
-    elif msgtype == 'warning':
-        logger.setLevel(logging.WARNING)
-        logger.warning(f'from [{sender}], {msg}')
-
-    elif msgtype == 'error':
-        logger.setLevel(logging.ERROR)
-        logger.error(f'from [{sender}], {msg}')
-
+    if _DEBUG is False:
+        pass
     else:
-        logger.setLevel(logging.WARNING)
-        logger.warning(f'from [{sender}], MSGTYPE does not exist')
+        if msgtype == 'debug':
+            logger.setLevel(logging.DEBUG)
+            logger.debug(f'from [{sender}], {msg}')
+
+        elif msgtype == 'info':
+            logger.setLevel(logging.INFO)
+            logger.info(f'from [{sender}], {msg}')
+
+        elif msgtype == 'warning':
+            logger.setLevel(logging.WARNING)
+            logger.warning(f'from [{sender}], {msg}')
+
+        elif msgtype == 'error':
+            logger.setLevel(logging.ERROR)
+            logger.error(f'from [{sender}], {msg}')
+
+        else:
+            logger.setLevel(logging.WARNING)
+            logger.warning(f'from [{sender}], MSGTYPE does not exist')
