628c45c6af8aa30a52fca7bf9637fc96dce62c2f
Cycrypto <jh01love@naver.com>
2021-09-20 20:04:26 +0900

    Add Label and handler

--- example/Agenda/ui-event.py
+++ example/Agenda/ui-event.py
@@ -1,6 +1,8 @@
 import sys
 import lms
-from lms import session
+import requests
+import lms.session as session
+from lms import _LOG as log
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
@@ -13,10 +15,21 @@ class MainWindow(QMainWindow, MAIN_FORM):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.setLoginInfo()
+        self.login: requests.Session = session.GetSession().session
+
+    def setLoginInfo(self):
+        try:
+            name, number, email = session.GetUserInfo(self.login).getInfo()
+            self.label.setText(f"{name}({number}) - {email}님 환영합니다")
+
+        except Exception as e:
+            log("info", "ui-event.setLoginInfo", f"{e}")
+            self.label.setText(f"세션에 접속하는데 실패했습니다")
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myWindow = LoginWindow()
+    myWindow = MainWindow()
     myWindow.show()
     app.exec()
